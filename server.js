26afa34b5e0dfce3ef939c019ae8723e90a24a48
aleisii <alexispir3l4xs@gmail.com>
2025-12-06 15:01:48 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servir archivos estáticos (tu index.html)
+app.use(express.static(path.join(__dirname, '/')));
+
+// Enviar index.html en cualquier ruta
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// ESCUCHAR EN 0.0.0.0 (Esto es lo que arregla el offline)
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
